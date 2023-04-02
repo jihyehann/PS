95e3b7ef5f01dabd50e3f3bbe578fe56e3d4d205
Jihye Han <haneh0714@gmail.com>
2023-04-02 16:26:30 +0900

    [level 2] Title: 프린터, Time: 0.70 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        List<Integer> print = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i=0; i<priorities.length; i++) {
+            pq.add(priorities[i]);
+        }
+        int cnt = 1;
+        boolean[] check = new boolean[priorities.length];
+        while (true) {
+            for (int i=0; i<priorities.length; i++) {
+                if (check[i]) continue;
+                if (pq.peek() == priorities[i]) {
+                    pq.remove();
+                    check[i] = true;
+                    if (i == location) return cnt;
+                    cnt++;
+                }
+            }
+        }
+    }
+}

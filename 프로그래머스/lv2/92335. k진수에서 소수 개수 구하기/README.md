c97916c321e8c9059eeb8550b2d9eb57c4d15ec4
Jihye Han <haneh0714@gmail.com>
2023-04-03 13:28:22 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.09 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.10 ms
+메모리: 74.9 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,23 +1,14 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        StringBuilder num = new StringBuilder();
-        while (n > 0) {
-            if (n%k == 0) {
-                if (isPrime(num.reverse().toString())) answer++;
-                num.setLength(0);
-            } else {
-                num.append(n%k);
-            }
-            n/=k;
+        String[] nums = Integer.toString(n, k).split("0");
+        for (String num : nums) {
+            if (!num.isEmpty() && isPrime(Long.parseLong(num))) answer++;
         }
-        if (isPrime(num.reverse().toString())) answer++;
         return answer;
     }
 
-    boolean isPrime(String strNum) {
-        if (strNum.isEmpty()) return false;
-        long num = Long.parseLong(strNum);
+    boolean isPrime(long num) {
         if (num < 2) return false;
         for (long i=2; i<=Math.sqrt(num); i++) {
             if (num % i == 0) return false;

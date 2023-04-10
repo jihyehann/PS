aa310378003420db0bb73781290c2398e8f86986
Jihye Han <haneh0714@gmail.com>
2023-04-10 13:41:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.95 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,13 +1,12 @@
 import java.util.*;
+import java.awt.*;
 
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visit = new boolean[n][m];
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0,0,1));
-        visit[0][0] = true;
+        q.add(new Point(0,0));
         int[][] mv = {{1,0},{-1,0},{0,1},{0,-1}};
         while (!q.isEmpty()) {
             Point now = q.remove();
@@ -15,26 +14,14 @@ class Solution {
                 int nx = now.x + mv[i][0];
                 int ny = now.y + mv[i][1];
                 if (nx == n-1 && ny == m-1) {
-                    return now.cnt+1;
+                    return maps[now.x][now.y]+1;
                 }
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visit[nx][ny] || maps[nx][ny] == 0) continue;
-                visit[nx][ny] = true;
-                q.add(new Point(nx, ny, now.cnt+1));
+                if (maps[nx][ny] == 0 || maps[nx][ny] > 1) continue;
+                maps[nx][ny] = maps[now.x][now.y] + 1;
+                q.add(new Point(nx, ny));
             }
         }
         return -1;
-    }
-    
-    class Point {
-        int x;
-        int y;
-        int cnt;
-        
-        public Point(int x, int y, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
     }
 }

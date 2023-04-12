a2406d7cb35268de9b76841b44e33f7c082eb7d9
Jihye Han <haneh0714@gmail.com>
2023-04-12 14:47:06 +0900

    [unrated] Title: 숫자 변환하기, Time: 1.65 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 class Solution {
+    Queue<Calc> q = new LinkedList<>();
+    boolean[] visit;
+    
     class Calc {
         int num;
         int cnt;
@@ -10,38 +13,27 @@ class Solution {
             this.cnt = cnt;
         }
     }
-    
     public int solution(int x, int y, int n) {
         if (x == y) return 0;
-        
-        Queue<Calc> q = new LinkedList<>();
-        boolean[] visit = new boolean[y+1];
+       
         q.add(new Calc(x, 0));
+        visit = new boolean[y+1];
         visit[x] = true;
         while(!q.isEmpty()) {
             Calc now = q.remove();
-            
-            int next = now.num + n;
-            if (next == y) return now.cnt+1;
-            if (next < y && !visit[next]) {
-                visit[next] = true;
-                q.add(new Calc(next, now.cnt+1));
-            }
-            
-            next = now.num*2;
-            if (next == y) return now.cnt+1;
-            if (next < y && !visit[next]) {
-                visit[next] = true;
-                q.add(new Calc(next, now.cnt+1));
-            }
-
-            next = now.num*3;
-            if (next == y) return now.cnt+1;
-            if (next < y && !visit[next]) {
-                visit[next] = true;
-                q.add(new Calc(next, now.cnt+1));
-            }
+            if (addQ(now.num + n, now.cnt+1, y)) return now.cnt+1;
+            if (addQ(now.num * 2, now.cnt+1, y)) return now.cnt+1;
+            if (addQ(now.num * 3, now.cnt+1, y)) return now.cnt+1;
         }
         return -1;
+    }
+    
+    boolean addQ(int num, int cnt, int y) {
+        if (num == y) return true;
+        if (num < y && !visit[num]) {
+            visit[num] = true;
+            q.add(new Calc(num, cnt));
+        }
+        return false;
     }
 }

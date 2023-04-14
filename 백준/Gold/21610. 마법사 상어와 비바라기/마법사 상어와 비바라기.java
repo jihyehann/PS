4148e615117c2bc4c47cac0a4a7f5f744189837e
Jihye Han <haneh0714@gmail.com>
2023-04-14 12:40:34 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 220 ms, Memory: 17200 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.awt.Point;
 import static java.lang.Integer.parseInt;
 
 public class Main {
     static int N;
     static int[][] mat;
-    static List<Point> cloud = new ArrayList<>(); // 구름 위치
+    static boolean[][] cloud; // 구름 위치
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,10 +20,11 @@ public class Main {
                 mat[i][j] = parseInt(input[j]);
             }
         }
-        cloud.add(new Point(N-1,0));
-        cloud.add(new Point(N-1,1));
-        cloud.add(new Point(N-2,0));
-        cloud.add(new Point(N-2,1));
+        cloud = new boolean[N][N];
+        cloud[N-1][0] = true;
+        cloud[N-1][1] = true;
+        cloud[N-2][0] = true;
+        cloud[N-2][1] = true;
         for (int i=0; i<M; i++) {
             input = br.readLine().split(" ");
             int d = parseInt(input[0])-1;
@@ -46,47 +45,56 @@ public class Main {
 
     static int[][] dir = {{0,-1}, {-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}}; // 홀수는 대각선
     static void move(int d, int s) {
-        for (Point p : cloud) {
-            int nx = p.x + dir[d][0]*s;
-            int ny = p.y + dir[d][1]*s;
-            if (nx < 0) nx += N;
-            else if (nx >= N) nx -= N;
-            if (ny < 0) ny += N;
-            else if (ny >= N) ny -= N;
+        boolean[][] next = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!cloud[i][j]) continue;
+                int nx = i + dir[d][0]*s;
+                int ny = j + dir[d][1]*s;
+                if (nx < 0) nx += N;
+                else if (nx >= N) nx -= N;
+                if (ny < 0) ny += N;
+                else if (ny >= N) ny -= N;
 
-            // 구름 이동
-            p.x = nx;
-            p.y = ny;
+                // 구름 이동
+                next[nx][ny] = true;
 
-            // 비 내리기
-            mat[nx][ny] += 1;
+                // 비 내리기
+                mat[nx][ny] += 1;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cloud[i][j] = next[i][j];
+            }
         }
     }
 
     static void copyWater() { // 물복사
-        for (Point p : cloud) {
-            int cnt = 0;
-            for (int j=1; j<8; j+=2) { // 대각선
-                int x = p.x + dir[j][0];
-                int y = p.y + dir[j][1];
-                if (x < 0 || x >= N || y < 0 || y >= N) continue;
-                if (mat[x][y] > 0) cnt++;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int cnt = 0;
+                if (!cloud[i][j]) continue;
+                for (int k=1; k<8; k+=2) {
+                    int x = i + dir[k][0];
+                    int y = j + dir[k][1];
+                    if (x < 0 || x >= N || y < 0 || y >= N) continue;
+                    if (mat[x][y] > 0) cnt++;
+                }
+                mat[i][j] += cnt;
             }
-            mat[p.x][p.y] += cnt;
         }
     }
 
     static void addCloud() {
-        Map<Point, Boolean> m = new HashMap<>();
-        for (Point p : cloud) {
-            m.put(p, true);
-        }
-        cloud.clear();
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
-                if (!m.containsKey(new Point(i,j)) && mat[i][j] >= 2) {
+                if (!cloud[i][j] && mat[i][j] >= 2) {
                     mat[i][j] -= 2;
-                    cloud.add(new Point(i,j));
+                    cloud[i][j] = true;
+                } else {
+                    cloud[i][j] = false;
                 }
             }
         }
